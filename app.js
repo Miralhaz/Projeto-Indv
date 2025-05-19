8a6678a2d56805da24b9c0235e343b2b38748ee7
Miralhaz <lucas.silva051@sptech.school>
2025-05-18 23:52:29 -0300

    Exclusão de rotas, models, controllers desnecessarios para o meu site

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
 var perguntasRouter = require("./src/routes/perguntas");
 var timesFavRouter = require("./src/routes/timesFav")
 
@@ -33,7 +32,6 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
 app.use("/perguntas", perguntasRouter);
 app.use("/timesFav", timesFavRouter);
 
